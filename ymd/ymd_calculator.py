127e13dfb2adb640ad9cb64953329d1261d20722
Kelvin <kktse@uwaterloo.ca>
2014-12-26 16:29:25 -0500

    Initial commit for ymd. Set up matrix to calculate forces as a function of slip angle.

--- ymd/ymd_calculator.py
+++ ymd/ymd_calculator.py
@@ -13,63 +13,45 @@ def tire_model(alpha):
 
 def main():
     # define test conditions
-    #beta = math.radians(1)  # degrees
-    #delta = math.radians(0)  # degrees
-    beta = 1
-    delta = 0
+    beta = math.radians(-10)
+    delta = math.radians(0)
 
     # define vehicle parameters
     wheelbase = 1000  # in milimetres [mm]
     trackwidth = 700  # in milimetres [mm]
     weightdist = .70  # percentage [%]
-    mass = 250  # kilograms [kg]
-    cornerstiff = 100
+    # mass = 250  # kilograms [kg]
+    cornerstiff = -50
 
     # calculate vehicle characteristics
     a = wheelbase * weightdist / 1000
     b = wheelbase/1000 - a
 
-    # solve for tire forces
-    force_f = tire_model(beta+delta)
-    force_r = tire_model(beta)
-
-    f_lat = force_f + force_r
-    a_lat = f_lat/mass
-    yaw = a*force_f - b*force_r
-
     # develop matrix
     # TODO: better naming perhaps?
     a11 = cornerstiff*math.sin(delta)
     a12 = cornerstiff*math.sin(delta)
-    a13 = cornerstiff
-    a14 = cornerstiff
+    a13 = 0
+    a14 = 0
     a21 = cornerstiff*math.cos(delta)
     a22 = cornerstiff*math.cos(delta)
-    a23 = 0
-    a24 = 0
+    a23 = cornerstiff
+    a24 = cornerstiff
     a31 = cornerstiff*(trackwidth/1000/2*math.sin(delta) + a*math.cos(delta))
     a32 = cornerstiff*(-trackwidth/1000/2*math.sin(delta) + a*math.cos(delta))
     a33 = cornerstiff*-b
     a34 = cornerstiff*-b
 
-    matrix = np.matrix([[a11, a12, a13, a14],
-                        [a21, a22, a23, a24],
-                        [a31, a32, a33, a34]])
+    matrix = np.matrix([[a11, a12, a13, a14],   # F_x
+                        [a21, a22, a23, a24],   # F_y
+                        [a31, a32, a33, a34]])  # M_z
 
-    slipangles = np.matrix('1;1;1;1')
+    # FR, FL, RR, RL
+    slipangles = np.matrix([[beta+delta], [beta+delta], [beta], [beta]])
 
     result = matrix*slipangles
+    print("Matrix Method - car model")
     print(result)
-
-    print(f_lat)
-    print(a_lat)
-    print(yaw)
-
-    print('I like turtles')
-    x = np.array([1, 2, 3])
-    print(x)
-    force_fr = tire_model(2)
-    print (force_fr)
 
 
 if __name__ == "__main__":
