155166511c2677a5fe3bde967e2320739a0aa7c0
Kelvin <kktse@uwaterloo.ca>
2014-12-29 22:09:31 -0500

    Successfully generated a YMD! TODO: Plot presentation, move the setup and config out of the script, aerodynamics

---
+++ ymd/model/pacejka94.py
@@ -0,0 +1,122 @@
+import math
+
+
+class Pacejka94:
+    ''' Pacejka 94 lateral tire model.
+        http://www.edy.es/dev/docs/pacejka-94-parameters-explained-a-comprehensive-guide/
+        http://www.optimumg.com/docs/OptimumTire_Help_File.pdf
+    '''
+
+    def __init__(self, coeff):
+        # Tire Coefficients
+        self.fnomin = coeff['fnomin']
+        self.pCy1 = coeff['pcy1']
+        self.pDy1 = coeff['pdy1']
+        self.pDy2 = coeff['pdy2']
+        self.pDy3 = coeff['pdy3']
+        self.pEy1 = coeff['pey1']
+        self.pEy2 = coeff['pey2']
+        self.pEy3 = coeff['pey3']
+        self.pEy4 = coeff['pey4']
+        self.pKy1 = coeff['pky1']
+        self.pKy2 = coeff['pky2']
+        self.pKy3 = coeff['pky3']
+        self.pHy1 = coeff['phy1']
+        self.pHy2 = coeff['phy2']
+        self.pHy3 = coeff['phy3']
+        self.pVy1 = coeff['pvy1']
+        self.pVy2 = coeff['pvy2']
+        self.pVy3 = coeff['pvy3']
+        self.pVy4 = coeff['pvy4']
+
+    # Calculate Pacejka coefficients, prepend with 'c' for 'coefficient'
+    def fy(self, f_z, alpha, gamma):
+        '''
+        Calculates tire force, F_y
+        where f_z    vertical tire load [N]
+              alpha  slip angle [deg]
+              gamma  camber angle [deg]
+        '''
+        h = self.cHy(f_z, gamma)
+        k = self.cKy(f_z, gamma)
+
+        c = self.pCy1
+        d = self.cDy(f_z, gamma)
+        e = self.cEy(f_z, alpha, gamma, h)
+        v = self.cVy(f_z, gamma)
+
+        b = self.cBy(k, c, d)
+        bx1 = self.cBx1(alpha, b, h)
+
+        return d * math.sin(c * math.atan(bx1 - e * (bx1 -
+                            math.atan(bx1)))) + v
+
+    def cDy(self, f_z, gamma):
+        '''
+        Calculates lateral peak factor, D
+        where f_z    vertical tire load [N]
+              gamma  camber angle [deg]
+        '''
+        return f_z * (self.pDy1 + self.pDy2 * self.fnorm(f_z)) * (1 - self.pDy3 * gamma * gamma)
+
+    def cKy(self, f_z, gamma):
+        '''
+        Calculates lateral stiffness, K
+        where f_z    vertical tire load [N]
+              gamma  camber angle [deg]
+        '''
+        return self.pKy1 * self.fnomin * math.sin(2 * math.atan(f_z / (self.pKy2
+                                                    * self.fnomin))) * (1 - self.pKy3 * math.fabs(gamma))
+
+    def cBy(self, cKy, cCy, cDy):
+        '''
+        Calculates lateral stiffness factor, B
+        where cKy    lateral stiffness, K
+              cCy    shape factor, C
+              cDy    peak factor, D
+        '''
+        return cKy/(cCy*cDy)
+
+    def cEy(self, f_z, gamma, alpha, yH):
+        '''
+        Calculates lateral curvature factor, E
+        where f_z    vertical tire load [N]
+              gamma  camber angle [deg]
+              alpha  slip angle [deg]
+              yH     horizontal shift, H
+        '''
+        sign = math.copysign(1, alpha + yH)
+        return (self.pEy1 + self.pEy2 * self.fnorm(f_z)) * (1 - (self.pEy4 * gamma + self.pEy3) * sign)
+
+    def cHy(self, f_z, gamma):
+        '''
+        Calculates lateral horizontal shift, H
+        where f_z    vertical tire load [N]
+              gamma  camber angle [deg]
+        '''
+        return self.pHy1 + self.pHy2 * self.fnorm(f_z) + self.pHy3 * gamma
+
+    def cVy(self, f_z, gamma):
+        '''
+        Calculates lateral vertical shift, V
+        where f_z    vertical tire load [N]
+              gamma  camber angle [deg]
+        '''
+        return (self.pVy1 + self.pVy2 * self.fnorm(f_z) + ((self.pVy3 + self.pVy4 *
+                                                            self.fnorm(f_z)) * gamma)) * f_z
+
+    def cBx1(self, alpha, cBy, cHy):
+        '''
+        Calculates ??? factor, Bx1
+        where alpha  slip angle [deg]
+              cBy    stiffness factor, B
+              cHy    horizontal shift, H
+        '''
+        return cBy * (alpha + cHy)
+
+    def fnorm(self, f_z):
+        '''
+        Calculates normalized vertical load, df_z
+        where f_z    vertical tire load [N]
+        '''
+        return (f_z - self.fnomin)/self.fnomin
--- ymd/ymd_calculator.py
+++ ymd/ymd_calculator.py
@@ -1,57 +1,136 @@
 import numpy as np
 import math
+import configparser
+import matplotlib.pyplot as plt
+
+from model.pacejka94 import Pacejka94 as pacejka
 
 
-def tire_model(alpha):
-    """ returns tire force generated at a given slip angle
-        input  - alpha (degree)
-        output - lateral tire force (N)
-    """
-    # assume 600N/degree cornering stiffness
-    return 100*alpha
-
-
-def main():
-    # define test conditions
-    beta = math.radians(-10)
-    delta = math.radians(0)
-
-    # define vehicle parameters
-    wheelbase = 1000  # in milimetres [mm]
-    trackwidth = 700  # in milimetres [mm]
-    weightdist = .70  # percentage [%]
-    # mass = 250  # kilograms [kg]
-    cornerstiff = -50
-
-    # calculate vehicle characteristics
-    a = wheelbase * weightdist / 1000
-    b = wheelbase/1000 - a
-
-    # develop matrix
-    # TODO: better naming perhaps?
-    a11 = cornerstiff*math.sin(delta)
-    a12 = cornerstiff*math.sin(delta)
+def solver(trackwidth, a, b, delta, fy_fr, fy_fl, fy_rr, fy_rl):
+    # Transformation matrix
+    a11 = math.sin(delta)
+    a12 = math.sin(delta)
     a13 = 0
     a14 = 0
-    a21 = cornerstiff*math.cos(delta)
-    a22 = cornerstiff*math.cos(delta)
-    a23 = cornerstiff
-    a24 = cornerstiff
-    a31 = cornerstiff*(trackwidth/1000/2*math.sin(delta) + a*math.cos(delta))
-    a32 = cornerstiff*(-trackwidth/1000/2*math.sin(delta) + a*math.cos(delta))
-    a33 = cornerstiff*-b
-    a34 = cornerstiff*-b
+    a21 = math.cos(delta)
+    a22 = math.cos(delta)
+    a23 = 1
+    a24 = 1
+    a31 = (trackwidth/2*math.sin(delta) + a*math.cos(delta))
+    a32 = (-trackwidth/2*math.sin(delta) + a*math.cos(delta))
+    a33 = -b
+    a34 = -b
 
     matrix = np.matrix([[a11, a12, a13, a14],   # F_x
                         [a21, a22, a23, a24],   # F_y
                         [a31, a32, a33, a34]])  # M_z
 
     # FR, FL, RR, RL
-    slipangles = np.matrix([[beta+delta], [beta+delta], [beta], [beta]])
+    forces = np.matrix([[fy_fr], [fy_fl], [fy_rr], [fy_rl]])
 
-    result = matrix*slipangles
-    print("Matrix Method - car model")
+    return matrix * forces
+
+
+def load_transfer(mass, accel, trackwidth, cg_height):
+    # Highly simplified model - neglect kinematics and suspension
+    return (9.81 * mass * cg_height * accel) / trackwidth
+
+
+def tire_load(mass, accel, trackwidth, cg_height, weightdist, aloadtrnsfrdist):
+    delta_load = load_transfer(mass, accel, trackwidth, cg_height)
+    f_trnsfr = delta_load * aloadtrnsfrdist
+    r_trnsfr = delta_load * (1 - aloadtrnsfrdist)
+
+    fz_fr = (9.81 * mass * weightdist / 2) - f_trnsfr
+    fz_fl = (9.81 * mass * weightdist / 2) + f_trnsfr
+    fz_rr = (9.81 * mass * (1 - weightdist) / 2) - r_trnsfr
+    fz_rl = (9.81 * mass * (1 - weightdist) / 2) + r_trnsfr
+
+    return fz_fr, fz_fl, fz_rr, fz_rl
+
+
+def main():
+    # define vehicle parameters
+    cgheight = 300/1000    # in milimetres [mm]
+    wheelbase = 1650/1000  # in milimetres [mm]
+    trackwidth = 1250/1000  # in milimetres [mm]
+    weightdist = .40  # percentage, front [%]
+    mass = 250  # kilograms [kg]
+    aloadtrnsfrdist = .35  # percentage, front [%]
+
+    # define test conditions
+    error = 0.0001
+    beta_sweep = np.arange(-8, 9)
+    beta_sweep = [math.radians(value) for value in beta_sweep]
+    delta_sweep = np.arange(-9, 10)
+    delta_sweep = [math.radians(value) for value in delta_sweep]
+
+    # read in tire data
+    config = configparser.ConfigParser()
+    config.read('data/tire/avon_fs_front.ini')
+    coeff = {key: float(config['lateral'][key]) for key in config['lateral']}
+
+    # build tire model
+    tire_model = pacejka(coeff)
+    sweep = np.linspace(-0.2, 0.2, 60)
+    result = [tire_model.fy(500, angle, 0) for angle in sweep]
     print(result)
+
+    # calculate vehicle characteristics
+    a = wheelbase * (1 - weightdist)
+    b = wheelbase - a
+
+    # static vertical load
+    staticfz_fr, staticfz_fl, staticfz_rr, staticfz_rl = tire_load(mass, 0, trackwidth, cgheight,
+                                                                   weightdist, aloadtrnsfrdist)
+
+    result_ay = np.empty([len(beta_sweep), len(delta_sweep)])
+    result_mz = np.empty([len(beta_sweep), len(delta_sweep)])
+
+    for i, beta in enumerate(beta_sweep):
+        for j, delta in enumerate(delta_sweep):
+            # Initialize vertical load variables
+            fz_fr = staticfz_fr
+            fz_fl = staticfz_fl
+            fz_rr = staticfz_rr
+            fz_rl = staticfz_rl
+
+            m_z = 0
+            a_lat = 0
+            a_lat_prev = 100
+
+            while math.fabs(a_lat - a_lat_prev) > error:
+                # TODO: cannot guarantee that the solver runs
+
+                # Initialize previous value
+                a_lat_prev = a_lat
+
+                # Calculate tire forces
+                fy_fr = -tire_model.fy(fz_fr, -(beta-delta), 0)
+                fy_fl = tire_model.fy(fz_fl, beta-delta, 0)
+                fy_rr = -tire_model.fy(fz_rr, -beta, 0)
+                fy_rl = tire_model.fy(fz_rl, beta, 0)
+
+                # Solve for resulting forces and moments
+                solution = solver(trackwidth, a, b, delta, fy_fr, fy_fl, fy_rr, fy_rl)
+
+                a_lat = solution.flat[1]/mass
+                m_z = solution.flat[2]
+
+                fz_fr, fz_fl, fz_rr, fz_rl = tire_load(mass, a_lat/9.81, trackwidth, cgheight,
+                                                       weightdist, aloadtrnsfrdist)
+
+            result_ay[i][j] = a_lat/9.81
+            result_mz[i][j] = m_z
+
+    plt.plot(result_ay[:][:], result_mz[:][:])
+    plt.plot(np.transpose(result_ay)[:][:],
+             np.transpose(result_mz)[:][:])
+    plt.show()
+
+    #np.savetxt('acceleration.csv', result_ay, delimiter=',')
+    #np.savetxt('yawmoment.csv', result_mz, delimiter=',')
+    plt.savefig('ymd.png')
 
 
 if __name__ == "__main__":
